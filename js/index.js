669dacd2e6ce51ee99ffa1fe90553cc71473ca89
Yug Khanna <yugkhanna@Yugs-MBP.lan>
2018-04-25 16:28:58 -0400

    Designs changed

--- js/index.js
+++
@@ -1,8 +0,0 @@
-// All of these buttons designed by me: Ahmed Beheiry
-
-// find me on 
-// My Website: http://www.ahmedbeheiry.tk/
-// FB: https://www.facebook.com/Ahmed.Beheiryy
-// Linkedin: https://eg.linkedin.com/in/ahmedbeheiry
-
-/********** Thanks for being here **********/
